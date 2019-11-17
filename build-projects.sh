ccd3108817321e5866983554ce472b66d86b4d48
Flávio Oliveira <flaoliveira@uolinc.com>
2019-11-16 22:41:14 -0300

    inclusão de clients

--- build-projects.sh
+++ build-projects.sh
@@ -4,17 +4,28 @@ WORKDIR=$(pwd)
 
 echo $WORKDIR
 
-echo "Building Help-point Discovery Server"
+echo "Building Help-point Client Module"
+cd $WORKDIR/helppoint-client
+mvn package
+
+echo "Building Help-point Configuration server Module"
+cd $WORKDIR/helppoint-configuration-server
+mvn package
+
+echo "Building Help-point Discovery Server Module"
 cd $WORKDIR/helppoint-discovery-server
 mvn package
 
-echo "Building Help-point Gateway"
+echo "Building Help-point Gateway Module"
 cd $WORKDIR/helppoint-gateway
 mvn package
 
-
-echo "Building Help-point Product"
+echo "Building Help-point Product Module"
 cd $WORKDIR/helppoint-product
+mvn package
+
+echo "Building Help-point Salesman Module"
+cd $WORKDIR/helppoint-salesman
 mvn package
 
 echo "Building Help-point WEB"
